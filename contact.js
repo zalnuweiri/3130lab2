5ea888cd90c1d0242274dbdf422348e74696d113
Zayd A <zy854413@dal.ca>
2024-05-29 09:58:15 -0700

    Directory shuffle

---
+++ contact.js
@@ -0,0 +1,14 @@
+// src/contact.js
+import React from 'react';
+
+const Contact = ({ visitCount }) => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Home page visited: {visitCount} times</p>
+      <p>Contact us at: zy854413@dal.ca</p>
+    </div>
+  );
+};
+
+export default Contact;
